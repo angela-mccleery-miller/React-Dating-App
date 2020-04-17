2ff429d99f8be6ea088fb65359d53ced969f6d94
jelarson <jesslarson93@gmail.com>
2020-04-16 18:49:23 -0600

    fixed homepage, login, new account page styles

--- react-front-end-dating-app/src/components/pages/home.js
+++ react-front-end-dating-app/src/components/pages/home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import UserContainer from '../users/user-container';
-import NewAccount from '../pages/create-new-account'
+// import NewAccount from '../pages/create-new-account'
+import "../../style/user-profile.scss";
+
 
 export default function () {
     return (
-        <div className="home"> 
-            <h1>Home</h1>     
+        <div className="home">
+            <h1>Home</h1>
 
-        <div className="home-content-wrapper">       
-            {/* <h1>Home</h1>      */}
+            <div className="home-content-wrapper">
+                {/* <h1>Home</h1>      */}
 
-           <UserContainer />
+                <UserContainer />
+            </div>
         </div>
-      </div>
     );
 }
--- react-front-end-dating-app/src/components/users/user-profile.js
+++ react-front-end-dating-app/src/components/users/user-profile.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
-export default class UserProfile extends Component{      
-    constructor(props){
+export default class UserProfile extends Component {
+    constructor(props) {
         super(props);
 
         this.state = {
-           
+
             isHot: this.props.item.hotScore,
             isNotHot: this.props.item.notScore
         };
@@ -15,10 +15,10 @@ export default class UserProfile extends Component{
     handleIsHot = () => {
         let scoreHot = parseInt(this.state.isHot) + 1
         fetch(
-            `https://jel-flask-dating-app-api.herokuapp.com/profile/${this.props.item.id}`, 
+            `https://jel-flask-dating-app-api.herokuapp.com/profile/${this.props.item.id}`,
             {
                 method: "PATCH",
-                headers: {"Content-Type": "application/json"},
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
                     hotScore: scoreHot.toString(),
                     notScore: this.state.isNotHot.toString()
@@ -34,10 +34,10 @@ export default class UserProfile extends Component{
     handleIsNotHot = () => {
         let scoreNotHot = parseInt(this.state.isNotHot) + 1
         fetch(
-            `https://jel-flask-dating-app-api.herokuapp.com/profile/${this.props.item.id}`, 
+            `https://jel-flask-dating-app-api.herokuapp.com/profile/${this.props.item.id}`,
             {
                 method: "PATCH",
-                headers: {"Content-Type": "application/json"},
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
                     notScore: scoreNotHot.toString(),
                     hotScore: this.state.isHot.toString()
@@ -48,48 +48,53 @@ export default class UserProfile extends Component{
                 isNotHot: scoreNotHot.toString()
             })
         })
-    }    
+    }
 
-    render(){    
-        const {id, name, password, description, gender, email, hotScore, notScore, profileImgUrl} = this.props.item;
+    render() {
+        const { id, name, password, description, gender, email, hotScore, notScore, profileImgUrl } = this.props.item;
 
-        return (     
+        return (
             <div className="profile-item-wrapper">
                 <div className="hotnothot-score-wrapper">
-                    <div>
-                        <p>Hot:  {this.state.isHot}</p>
-                    </div>
-                    <div>
-                        <p>not Hot:  {this.state.isNotHot}</p>
+                    <div className='scores'>
+
+                        <div className='score-item'>
+                            <p>HOT:  {this.state.isHot}</p>
+                        </div>
+                        <div className='score-item'>
+                            <p>NOT:  {this.state.isNotHot}</p>
+                        </div>
                     </div>
                 </div>
 
                 <div className="img-profile-wrapper">
                     <div>
                         <img src={profileImgUrl} />
-                    </div>                    
+                    </div>
                 </div>
 
                 <div className="user-info-wrapper">
-
-                  <h4>{name}</h4>
-                    {/* <p>Name: {name}</p> */}
-                    <p>Gender: {gender}</p>
-                    <p>Email: {email}</p>
-                    <p>Description: {description}</p>                   
+                    <div className='user-name'>
+                        <h4>{name}</h4>
+                    </div>
+                    <div className='user-info'>
+                        <p>Gender: {gender}</p>
+                        <p>Email: {email}</p>
+                        <p>Description: {description}</p>
+                    </div>
                 </div>
 
                 <div className="btn-wrapper">
-                    <button onClick={this.handleIsHot}>Is HOT!</button>
-                    <button onClick={this.handleIsNotHot}>Is NOT Hot!</button>
-                    <button>View Profile</button> 
-                    <style>width="12" height="10"</style>
-            </div>                
-          </div>
+                    <button className='profile-button' onClick={this.handleIsHot}>They are HOT!</button>
+                    <button className='profile-button' >View Profile</button>
+                    <button className='profile-button' onClick={this.handleIsNotHot}>They are NOT!</button>
+                    {/* <style>width="12" height="10"</style> */}
+                </div>
+            </div>
 
         );
     }
-} 
+}
 
 
 
@@ -134,4 +139,3 @@ export default class UserProfile extends Component{
             //         <div className="subtitle">{description}</div>
             //     </div>
             // </div>        
-        
